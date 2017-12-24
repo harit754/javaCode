9b69a357a8ab75f066f497112d92686697aa0e25
harit754 <haritsaini754@gmail.com>
2017-12-24 09:37:24 +0530

    Integer graph implementaion

--- src/GraphTheory/graph3.java
+++ src/GraphTheory/graph3.java
@@ -1,30 +1,63 @@
 package GraphTheory;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
+//adjacency list
+//InTeger TYPE Vertexes----->simple graph
+
 public class graph3 {
-	public static void main(String[] args) {
 	
-		Scanner s=new Scanner(System.in);
-		int v=s.nextInt();
-		ArrayList<ArrayList<Integer>> list=new ArrayList<>(v);
-		
+
+	int V;
+	
+	//array of linked list
+	List<Integer> l[];
+	
+	//n--->no of vertexes
+	graph3(int v){
+		V=v;
+		l=new LinkedList[v];
 		for(int i=0;i<v;i++){
-//			list.get(i)=new ArrayList<Integer>();
-//			list.set(i, new ArrayList<Integer>());
-		}
-		int e=s.nextInt();
-		while(e>0){
-			int v1=s.nextInt();
-			int v2=s.nextInt();
-			list.get(v1).add(v2);
-			list.get(v2).add(v1);
-			
-			
-			
-			e--;
+			l[i]=new LinkedList<>();
 		}
 	}
-
+	
+	public void addEdge(int u,int v,boolean bidir){
+		l[u].add(v);
+		if(bidir){
+			l[v].add(u);
+		}
+	}
+	
+	//overRide function
+	public String toString(){
+		String res="";
+		for(int i=0;i<V;i++){
+			res += i+":";
+			for(int a:l[i]){
+				res +=" "+a;
+			}
+//			System.out.println();
+			res += "\n";
+		}
+		return res;
+		
+	}
+	
+	public static void main(String[] args) {
+		graph3 g=new graph3(3);
+		g.addEdge(0,2,true);
+		g.addEdge(0, 1, true);
+		
+		System.out.println(g);
+		
+//		List<Integer> list=new LinkedList<>();
+		
+	}
+	
+	
+	
 }
